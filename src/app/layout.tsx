fea5b3ca5c095d243a819b630d8ad3d98e2088a3
Lietson Dos Santos <lietsondosantos.dev@gmail.com>
2025-12-08 01:58:38 +0100

    chore(setup): Configure Husky, Biome, lint-staged, git-cz and commitlint for code quality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "IPLookup",
+  title: 'IPLookup',
   description:
-    "IPLookup oferece análise avançada de endereços IP com dados geográficos, rede, reputação e provedor. Rápido, preciso e ideal para equipas técnicas.",
-};
+    'IPLookup oferece análise avançada de endereços IP com dados geográficos, rede, reputação e provedor. Rápido, preciso e ideal para equipas técnicas.'
+}
 
-type Props = Readonly<{ children: React.ReactNode }>;
+type Props = Readonly<{ children: React.ReactNode }>
 
 export default function RootLayout({ children }: Props) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
-  );
+  )
 }
